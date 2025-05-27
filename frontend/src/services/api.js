de875d063f4f7a5532c3c2af889d91f1c112b9cb
yarkinovweb <152738033+yarkinovweb@users.noreply.github.com>
2025-05-27 20:33:46 +0500

    s
    s

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8002/api",
+  baseURL: "http://yarkinov.robohouse.tech/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
